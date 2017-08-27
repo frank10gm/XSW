9a832c41ce5fd20d8095de0e40e00949cb59150e
frank10gm <frank10gm@gmail.com>
2017-08-27 18:25:00 +0200

    ok

--- StritWalk/ViewModels/RegisterPageViewModel.cs
+++ StritWalk/ViewModels/RegisterPageViewModel.cs
@@ -1,10 +1,59 @@
 ﻿using System;
+using System.Threading.Tasks;
+using System.Windows.Input;
+using Xamarin.Forms;
+
 namespace StritWalk
 {
     public class RegisterPageViewModel : BaseViewModel
     {
+
+        //variables
+        public ICommand SignUpCommand { get; }
+
+        //properties
+        string message = string.Empty;
+        public string Message
+        {
+            get { return message; }
+            set { message = value; OnPropertyChanged(); }
+        }
+
         public RegisterPageViewModel()
         {
+            SignUpCommand = new Command(async () => await SignUp());
+        }
+
+        async Task SignUp()
+        {
+            try
+            {
+                IsBusy = true;
+                Message = "Loading...";
+
+                // Log the user in
+                result = await TryLoginAsync();
+            }
+            finally
+            {
+                Message = string.Empty;
+                IsBusy = false;
+                if (!result)
+                {
+                    string mex = "Wrong username or password...";
+                    for (var i = 0; i < mex.Length; i++)
+                    {
+                        Message += mex[i];
+                        await Task.Delay(100);
+                    }
+                    await Task.Delay(500);
+                    Message = string.Empty;
+                }
+
+
+                if (Settings.IsLoggedIn)
+                    App.GoToMainPage();
+            }
         }
     }
 }
