e038278c0e735d09a172365bd8d7a5dd41fa9967
frank10gm <frank10gm@gmail.com>
2017-08-31 11:34:44 +0200

    ok

--- Droid/MainActivity.cs
+++ Droid/MainActivity.cs
@@ -32,7 +32,7 @@ namespace StritWalk.Droid
                 var statusBarHeightInfo = typeof(FormsAppCompatActivity).GetField("_statusBarHeight", System.Reflection.BindingFlags.Instance | System.Reflection.BindingFlags.NonPublic);
                 statusBarHeightInfo.SetValue(this, 0);
                 //Window.SetStatusBarColor(new Android.Graphics.Color(18, 52, 86, 255));
-                Window.SetStatusBarColor(new Android.Graphics.Color(72,133,237));                
+                Window.SetStatusBarColor(new Android.Graphics.Color(25,118,210));                
             }
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
