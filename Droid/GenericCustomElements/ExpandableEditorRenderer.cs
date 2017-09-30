375df292b7c269a9d3c9c0381e670e39c208f2b9
frank10gm <frank10gm@gmail.com>
2017-09-30 12:42:27 +0200

    ok

--- Droid/GenericCustomElements/ExpandableEditorRenderer.cs
+++ Droid/GenericCustomElements/ExpandableEditorRenderer.cs
@@ -7,24 +7,46 @@ using StritWalk.Droid;
 [assembly: ExportRenderer(typeof(ExpandableEditor), typeof(ExpandableEditorRenderer))]
 namespace StritWalk.Droid
 {
-	public class ExpandableEditorRenderer : EditorRenderer
-	{
-		ExpandableEditor element;
-		protected override void OnElementChanged(ElementChangedEventArgs<Editor> ev)
-		{
-			base.OnElementChanged(ev);
+    public class ExpandableEditorRenderer : EditorRenderer
+    {
+        ExpandableEditor element;
 
-			if (Element == null)
-				return;
+        protected override void OnElementChanged(ElementChangedEventArgs<Editor> e)
+        {
+            base.OnElementChanged(e);
 
-			element = (ExpandableEditor)Element;
+            if (Element == null)
+                return;
 
-			Control.Hint = element.Placeholder;
-			Control.SetHintTextColor(element.PlaceholderColor.ToAndroid());
-			Control.TextChanged += (object sender, Android.Text.TextChangedEventArgs e) =>
-			{
-				Control.Hint = element.Placeholder;
-			};
-		}
-	}
+            if (e.OldElement != null)
+            {
+
+            }
+
+            if (e.NewElement != null)
+            {
+                element = (ExpandableEditor)Element;
+                Control.Hint = element.Placeholder;
+                Control.SetHintTextColor(element.PlaceholderColor.ToAndroid());
+
+                Control.TextChanged += (object sender, Android.Text.TextChangedEventArgs e1) =>
+                {
+                    
+                    Control.Hint = element.Placeholder;
+                    //Element.LayoutTo(new Rectangle(Element.X, Element.Y - Element.Height, Element.Width, Element.Height * 2));
+                };
+
+                Control.LayoutParameters = new LayoutParams(LayoutParams.MatchParent, LayoutParams.WrapContent);
+                Control.InputType = Android.Text.InputTypes.TextFlagMultiLine;
+
+            }
+        }
+
+        protected override void OnElementPropertyChanged(object sender, System.ComponentModel.PropertyChangedEventArgs e)
+        {
+            base.OnElementPropertyChanged(sender, e);
+
+            //Control.SetMaxLines(4);
+        }
+    }
 }
