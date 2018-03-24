5d497bf170d7572483494c56429007f41b1cfaca
frank10gm <frank10gm@gmail.com>
2018-03-24 19:30:25 +0100

    ok

---
+++ StritWalk/Views/PostPage.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+using Xamarin.Forms;
+
+namespace StritWalk.Views
+{
+    public class PostPage : ContentPage
+    {
+        public PostPage()
+        {
+            Content = new StackLayout
+            {
+                Children = {
+                    new Label { Text = "Hello ContentPage" }
+                }
+            };
+        }
+    }
+}
+
