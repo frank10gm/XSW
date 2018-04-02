4cf3403be51ac6aebc3c5502bb64a9c2d41b4057
frank10gm <frank10gm@gmail.com>
2018-04-02 11:37:35 +0200

    ok

--- iOS/Services/AudioPlayer.cs
+++ iOS/Services/AudioPlayer.cs
@@ -11,6 +11,7 @@ using static CoreFoundation.DispatchSource;
 using CoreFoundation;
 using UIKit;
 using System.Diagnostics;
+using System.Threading.Tasks;
 
 [assembly: Dependency(typeof(StritWalk.iOS.AudioPlayer))]
 
@@ -119,7 +120,7 @@ namespace StritWalk.iOS
 
         public void InitRecord()
         {
-            
+
         }
 
         public string StartRecording(double time)
@@ -161,8 +162,27 @@ namespace StritWalk.iOS
             recorder = AVAudioRecorder.Create(url, settings, out error);
             recorder.FinishedRecording += Recorder_FinishedRecording;
             recorder.PrepareToRecord();
+            recorder.MeteringEnabled = true;
             recorder.RecordFor(time);
+            Console.WriteLine("before start task");
+            Task task = Task.Run(() =>
+            {
+                Console.WriteLine("start task");
+                audioMetering();
+            });
+            task.Start();
             return audioFilePath;
+        }
+
+        private void audioMetering()
+        {
+            Console.WriteLine(recorder.Recording);
+            while (recorder.Recording)
+            {
+                recorder.UpdateMeters();
+                Console.WriteLine(string.Format("@@@ peak: {0} - average: {1}", recorder.PeakPower(0), recorder.AveragePower(0)));
+            }
+
         }
 
         public void StopRecording()
