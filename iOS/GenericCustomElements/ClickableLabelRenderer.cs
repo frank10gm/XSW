323d10de69f7f2021daa96613b91047b298fff20
frank10gm <frank10gm@gmail.com>
2018-04-08 12:24:42 +0200

    ok

--- iOS/GenericCustomElements/ClickableLabelRenderer.cs
+++ iOS/GenericCustomElements/ClickableLabelRenderer.cs
@@ -11,6 +11,7 @@ using Xamarin.Forms;
 using Xamarin.Forms.Platform.iOS;
 using System.ComponentModel;
 using CoreGraphics;
+using System.Diagnostics;
 
 [assembly: ExportRenderer(typeof(ClickableLabel), typeof(ClickableLabelRenderer))]
 namespace StritWalk.iOS
@@ -26,24 +27,24 @@ namespace StritWalk.iOS
 
             var firstAttributes = new UIStringAttributes
             {
-                ForegroundColor = UIColor.Blue,
-                BackgroundColor = UIColor.Yellow,
-                Font = UIFont.FromName("Courier", 18f),
-                Link = new NSUrl(view.FormattedText.ToString())
+                ForegroundColor = UIColor.Blue,                                
+                Link = new NSUrl("www.google.it")
             };
-            var attributedString = new NSMutableAttributedString(view.FormattedText.ToString());
-            attributedString.SetAttributes(firstAttributes, new NSRange(0, 10));
+            var attributedString = new NSMutableAttributedString(" <a href= http://www.google.com>Visit Google</a> ");
+            attributedString.SetAttributes(firstAttributes.Dictionary, new NSRange(0, 5));
 
             UITextView textView = new UITextView(new CGRect(0, 0, view.Width, view.Height));
             //textView.Text = view.FormattedText.ToString();   
             textView.AttributedText = attributedString;
+            
                     
             //Console.WriteLine(string.Format("@@@@ text : {0}", view.Text));
             textView.Font = UIFont.SystemFontOfSize((float)view.FontSize);
             textView.Editable = false;
             textView.Bounces = false;
             textView.Selectable = true;
-            textView.ShouldInteractWithUrl += ShouldInteractWithUrl;
+            textView.UserInteractionEnabled = true;            
+            textView.ShouldInteractWithUrl = ShouldInteractWithUrl;
 
             // Setting the data detector types mask to capture all types of link-able data
             textView.DataDetectorTypes = UIDataDetectorType.All;
@@ -53,38 +54,39 @@ namespace StritWalk.iOS
             SetNativeControl(textView);
         }
 
-        protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
-        {
-            base.OnElementPropertyChanged(sender, e);
+        //protected override void OnElementPropertyChanged(object sender, PropertyChangedEventArgs e)
+        //{
+        //    base.OnElementPropertyChanged(sender, e);
 
-            var view = (ClickableLabel)Element;
-            if (view == null) return;
+        //    var view = (ClickableLabel)Element;
+        //    if (view == null) return;
 
-            UITextView textView;
+        //    UITextView textView;
 
-            if (Control == null)
-            {
-                textView = new UITextView();
-                SetNativeControl(textView);
-            }
-            else if (e.PropertyName == Label.TextProperty.PropertyName)
-            {
-                if (Element != null && !string.IsNullOrWhiteSpace(Element.Text))
-                {
-                    var attr = new NSAttributedStringDocumentAttributes();
-                    var nsError = new NSError();
-                    attr.DocumentType = NSDocumentType.HTML;
+        //    if (Control == null)
+        //    {
+        //        textView = new UITextView();
+        //        SetNativeControl(textView);
+        //        textView.ShouldInteractWithUrl = ShouldInteractWithUrl;
+        //    }
+        //    else if (e.PropertyName == Label.TextProperty.PropertyName)
+        //    {
+        //        if (Element != null && !string.IsNullOrWhiteSpace(Element.Text))
+        //        {
+        //            var attr = new NSAttributedStringDocumentAttributes();
+        //            var nsError = new NSError();
+        //            attr.DocumentType = NSDocumentType.HTML;
 
-                    var myHtmlData = NSData.FromString(view.Text, NSStringEncoding.Unicode);
-                    Control.AttributedText = new NSAttributedString(myHtmlData, attr, ref nsError);
-                }
-            }
-        }
+        //            var myHtmlData = NSData.FromString(view.Text, NSStringEncoding.Unicode);
+        //            Control.AttributedText = new NSAttributedString(myHtmlData, attr, ref nsError);
+        //        }
+        //    }
+        //}
 
         bool ShouldInteractWithUrl(UITextView arg1, NSUrl arg2, NSRange arg3)
         {
-            Console.WriteLine(string.Format("@@@ pannita tesoroosa"));
-            return false;
+            Console.WriteLine("@@@ pannita tesoroosa");
+            return true;
         }
     }
 }
